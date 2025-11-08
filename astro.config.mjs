4d5b2ced4d68ecee02f07030abc778d573f6e5a1
blagoje7 <stojsavljevic.blagoje@gmail.com>
2025-11-08 14:49:34 +0100

    optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        }
+      }
+    }
+  },
+  build: {
+    inlineStylesheets: 'auto',
+  }
+});
